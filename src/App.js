6640c0b7f86f9afcb6269739b2737b06ea57c913
TheyCallMeCheng <cheng-corp@protonmail.com>
2022-08-31 18:00:18 +0200

    Added allcryptojobs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import JobsListings from "./components/JobListings"
-import jobsArray from "./jobs.json"
+import jobsArray from "./AllCryptoJobs.json"
 import Header from "./components/Header";
 import Search from './components/Search';
 
